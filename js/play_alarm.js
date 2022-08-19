0ec40b3dc7b6a9a5887cba9231cf951b82da6ef5
Glaucos Ginez <glaucosginez@gmail.com>
2022-08-20 00:51:49 +0100

    test

---
+++ js/play_alarm.js
@@ -0,0 +1,21 @@
+﻿var alarmSound = null;
+
+function playAlarm() {
+    createAlarmIfNeeded();
+    alarmSound.play()    
+    console.log("alarm played");
+}
+
+function stopAlarm() {
+    createAlarmIfNeeded();
+    alarmSound.stop();
+    console.log("alarm stoped");
+}
+
+function createAlarmIfNeeded() {
+    alarmSound = alarmSound || new Howl({
+        src: ['alarmwatch.mp3'],
+        volume: 0.5,
+        loop: true
+    });
+}
